cfc5b08facb732ba76374bc0782239749776dd2c
nhathuych <nhathuych@gmail.com>
2024-11-02 19:13:22 +0700

    add new task event

--- src/components/TodoFrame.jsx
+++ src/components/TodoFrame.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import todoIcon from '../assets/todo_icon.png'
 import TodoItem from './TodoItem'
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([])
+  const inputRef = useRef() // mapping between input and this variable
+
+  const addNewTask = () => {
+    const inputText = inputRef.current.value.trim()
+    if (inputText === '') {
+      resetInput()
+      return null
+    }
+
+    const todoItem = {
+      id: Date.now(),
+      text: inputText,
+      isCompleted: false
+    }
+
+    setTodoList((prev) => [...prev, todoItem])
+    resetInput()
+  }
+
+  const resetInput = () => {
+    inputRef.current.value = ''
+    inputRef.current.focus()
+  }
+
   return (
     <div className='flex flex-col bg-white rounded-xl place-self-center max-w-md w-11/12 p-7 min-h-[550px]'>
       {/* title */}
@@ -14,16 +39,25 @@ const Todo = () => {
       {/* input box */}
       <div className='flex items-center bg-gray-200 rounded-full my-7'>
         <input
+          ref={inputRef}
+          onKeyDown={(e) => { if (e.key === 'Enter') { addNewTask() } }}
           type='text'
           placeholder='Add a new task...'
           className='bg-transparent outline-none border-0 flex-1 placeholder:text-slate-600 h-14 pl-6 pr-2'
         />
-        <button className='bg-sky-800 text-white text-lg font-medium rounded-full border-none cursor-pointer w-32 h-14'>Add</button>
+        <button
+          onClick={addNewTask}
+          className='bg-sky-800 text-white text-lg font-medium rounded-full border-none cursor-pointer w-32 h-14'
+        >
+          Add
+        </button>
       </div>
 
       {/* todo list */}
       <div>
-        <TodoItem text="Learn ReactJS"/>
+        {todoList.map((todo, index) => {
+          return <TodoItem key={index} text={todo.text} />
+        })}
       </div>
     </div>
   )
